2692901c548733d1e76b82b55c2ccabdac7dc0f3
JLXW <jason9829@live.com>
2018-12-19 21:05:18 +0800

    make correction on shifting definition.

--- Inc/DMA.h
+++ Inc/DMA.h
@@ -56,9 +56,9 @@ struct dmaRegs{
 #define DMA_PFCTRLD  (0 << 5)		// Set DMA to flow controller
 
 #define DMA_DIR_PtM (00 << 6)		// Set data transfer direction to Peripheral to Memory
-#define DMA_DIR_MtP (01 << 6)		// Set data transfer direction to Memory to Peripheral
-#define DMA_DIR_MtM (10 << 6)		// Set data transfer direction to Memory to Memory
-#define DMA_DIR_REV (11 << 6)		// Reserved
+#define DMA_DIR_MtP (1 << 6)		// Set data transfer direction to Memory to Peripheral
+#define DMA_DIR_MtM (2 << 6)		// Set data transfer direction to Memory to Memory
+#define DMA_DIR_REV (3 << 6)		// Reserved
 
 #define DMA_CIRC	(1 << 8)		// Enable Circular mode
 #define DMA_CIRCD	(0 << 8)		// Disable Circular mode
@@ -70,22 +70,22 @@ struct dmaRegs{
 #define DMA_MINCD	(0 << 10)		// Disable Memory address pointer increment after each data transfer
 
 #define DMA_PSIZE8   (00 << 11)		// Set peripheral data size to 8-bit
-#define DMA_PSIZE16  (01 << 11)		// Set peripheral data size to 16-bit
-#define DMA_PSIZE32  (10 << 11)		// Set peripheral data size to 32-bit
-#define DMA_PSIZER   (11 << 11)		// Reserved
+#define DMA_PSIZE16  (1 << 11)		// Set peripheral data size to 16-bit
+#define DMA_PSIZE32  (2 << 11)		// Set peripheral data size to 32-bit
+#define DMA_PSIZER   (3 << 11)		// Reserved
 
 #define DMA_MSIZE8   (00 << 13)		// Set memory data size to 8-bit
-#define DMA_MSIZE16  (01 << 13)		// Set memory data size to 16-bit
-#define DMA_MSIZE32  (10 << 13)		// Set memory data size to 32-bit
-#define DMA_MSIZER   (11 << 13)		// Reserved
+#define DMA_MSIZE16  (1 << 13)		// Set memory data size to 16-bit
+#define DMA_MSIZE32  (2 << 13)		// Set memory data size to 32-bit
+#define DMA_MSIZER   (3 << 13)		// Reserved
 
 #define DMA_PINCOS   (0 << 15)		// Peripheral increment offset size is PSIZE
 #define DMA_PINCOSF  (1 << 15)		// Peripheral increment offset size is 4 bytes (fixed)
 
 #define DMA_PLL		 (00 << 16)     // Set the priority level to low
-#define DMA_PLM		 (01 << 16)     // Set the priority level to medium
-#define DMA_PLH		 (10 << 16)     // Set the priority level to high
-#define DMA_PLVH	 (11 << 16)     // Set the priority level to very high
+#define DMA_PLM		 (1 << 16)     // Set the priority level to medium
+#define DMA_PLH		 (2 << 16)     // Set the priority level to high
+#define DMA_PLVH	 (3 << 16)     // Set the priority level to very high
 
 #define DMA_DBME	(1 << 18)		// Enable Double buffer mode
 #define DMA_DBMD	(1 << 18)		// Disable Double buffer mode
@@ -94,14 +94,14 @@ struct dmaRegs{
 #define DMA_CT1		(1 << 19)		// Current target Memory 1
 
 #define DMA_PBURSTS  (00 << 21)     // Single transfer
-#define DMA_PBURST4  (01 << 21)     // Incremental burst of 4 beats
-#define DMA_PBURST8  (10 << 21)     // Incremental burst of 8 beats
-#define DMA_PBURST16 (11 << 21)     // Incremental burst of 16 beats
+#define DMA_PBURST4  (1 << 21)     // Incremental burst of 4 beats
+#define DMA_PBURST8  (2 << 21)     // Incremental burst of 8 beats
+#define DMA_PBURST16 (3 << 21)     // Incremental burst of 16 beats
 
-#define DMA_MBURSTS  (00 << 23)		// Single transfer
-#define DMA_MBURST4  (01 << 23)     // Incremental burst of 4 beats
-#define DMA_MBURST8  (10 << 23)     // Incremental burst of 8 beats
-#define DMA_MBURST16 (11 << 23)     // Incremental burst of 16 beats
+#define DMA_MBURSTS  (00 << 25)		// Single transfer
+#define DMA_MBURST4  (1 << 25)     // Incremental burst of 4 beats
+#define DMA_MBURST8  (2 << 25)     // Incremental burst of 8 beats
+#define DMA_MBURST16 (3 << 25)     // Incremental burst of 16 beats
 
 
 #define DMA_CH0SEL 	(000 << 24)		// Channel 0 selected
